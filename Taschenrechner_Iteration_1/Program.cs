e9422f311d90d45a1a92c7f7ab22a76063d97f38
Scheffcat <patrick.reiss@gmail.com>
2021-05-13 22:15:13 +0200

    Projektdateien hinzufügen.

---
+++ Taschenrechner_Iteration_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Taschenrechner_Iteration_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // User Story "Addieren": Als Benutzer möchte ich zwei Zahlen eingeben, um deren Summe berechnen zu lassen.
+
+            Console.Write("Bitte gibt den ersten Summanden ein: ");
+            string ersterSummand = Console.ReadLine();
+            Console.Write("Bitte gib den zweiten Summanden ein: ");
+            string zweiterSummand = Console.ReadLine();
+
+            // Wandel den Text in Ganzzahlen
+            int ersterSummandAlsZahl = Convert.ToInt32(ersterSummand);
+            int zweiterSummandAlsZahl = Convert.ToInt32(zweiterSummand);
+
+            // Berechnung ausführen
+            int summe = ersterSummandAlsZahl + zweiterSummandAlsZahl;
+
+            // Ausgabe
+            Console.WriteLine("Das Ergebnis lautet: {0}", summe);
+            Console.ReadLine();
+        }
+    }
+
+}
